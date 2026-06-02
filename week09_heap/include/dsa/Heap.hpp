6c63c59ceebfc12de79271c540e37b25521ee3dd
ahadh <awa3eq3w4@aeswe.lt>
2026-06-02 15:36:39 +0300

    Week 8 Task Completed

--- week09_heap/include/dsa/Heap.hpp
+++ week09_heap/include/dsa/Heap.hpp
@@ -5,6 +5,7 @@
 #pragma once
 #include <cstddef>
 #include <stdexcept>
+#include <utility>
 #include "dsa/DynamicArray.hpp"
 
 namespace dsa {
@@ -19,32 +20,76 @@ namespace dsa {
         std::size_t size() const { return data_.size(); }
 
         const T& top() const {
-            if (empty()) throw std::out_of_range("Heap::top on empty heap");
+            if (empty())
+                throw std::out_of_range("Heap::top on empty heap");
+
             return data_[0];
         }
 
         void push(const T& value) {
-            // TODO: push_back then sift_up
-            (void)value;
+            data_.push_back(value);
+            sift_up(data_.size() - 1);
         }
 
         void pop() {
-            // TODO: swap root with last, pop_back, sift_down
+            if (empty())
+                throw std::out_of_range("Heap::pop on empty heap");
+
+            if (data_.size() == 1) {
+                data_.pop_back();
+                return;
+            }
+
+            std::swap(data_[0], data_[data_.size() - 1]);
+            data_.pop_back();
+            sift_down(0);
         }
 
     private:
-        static std::size_t parent(std::size_t i) { return (i - 1) / 2; }
-        static std::size_t left(std::size_t i) { return 2 * i + 1; }
-        static std::size_t right(std::size_t i) { return 2 * i + 2; }
+        static std::size_t parent(std::size_t i) {
+            return (i - 1) / 2;
+        }
+
+        static std::size_t left(std::size_t i) {
+            return 2 * i + 1;
+        }
+
+        static std::size_t right(std::size_t i) {
+            return 2 * i + 2;
+        }
 
         void sift_up(std::size_t i) {
-            // TODO
-            (void)i;
+            while (i > 0) {
+                std::size_t p = parent(i);
+
+                if (data_[p] >= data_[i])
+                    break;
+
+                std::swap(data_[p], data_[i]);
+                i = p;
+            }
         }
 
         void sift_down(std::size_t i) {
-            // TODO
-            (void)i;
+            std::size_t n = data_.size();
+
+            while (true) {
+                std::size_t largest = i;
+                std::size_t l = left(i);
+                std::size_t r = right(i);
+
+                if (l < n && data_[l] > data_[largest])
+                    largest = l;
+
+                if (r < n && data_[r] > data_[largest])
+                    largest = r;
+
+                if (largest == i)
+                    break;
+
+                std::swap(data_[i], data_[largest]);
+                i = largest;
+            }
         }
 
     private:
--- week09_heap/include/dsa/Sorting.hpp
+++ week09_heap/include/dsa/Sorting.hpp
@@ -3,29 +3,52 @@
 //
 
 #pragma once
-#include <cstddef>
+#include <utility>
 
 namespace dsa {
 
     // heapify_down: fixes heap property in arr[0..n-1] starting at i
     template <class T>
     void heapify_down(T* arr, int n, int i) {
-        // TODO
-        (void)arr; (void)n; (void)i;
+
+        while (true) {
+            int largest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if (left < n && arr[left] > arr[largest])
+                largest = left;
+
+            if (right < n && arr[right] > arr[largest])
+                largest = right;
+
+            if (largest == i)
+                break;
+
+            std::swap(arr[i], arr[largest]);
+            i = largest;
+        }
     }
 
     // build_heap: bottom-up O(n)
     template <class T>
     void build_heap(T* arr, int n) {
-        // TODO
-        (void)arr; (void)n;
+
+        for (int i = n / 2 - 1; i >= 0; --i) {
+            heapify_down(arr, n, i);
+        }
     }
 
     // heapsort: O(n log n)
     template <class T>
     void heapsort(T* arr, int n) {
-        // TODO
-        (void)arr; (void)n;
+
+        build_heap(arr, n);
+
+        for (int end = n - 1; end > 0; --end) {
+            std::swap(arr[0], arr[end]);
+            heapify_down(arr, end, 0);
+        }
     }
 
 } // namespace dsa
