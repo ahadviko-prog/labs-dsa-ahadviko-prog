f7d3f6ec76c1fc2e79aa590f130f0d5f6f10fb8d
ahadh <awa3eq3w4@aeswe.lt>
2026-06-02 11:19:56 +0300

    Week 8 Task Completed

--- week08_quicksort/include/dsa/Sorting.hpp
+++ week08_quicksort/include/dsa/Sorting.hpp
@@ -7,19 +7,41 @@
 
 namespace dsa {
 
-    // Choose ONE partition scheme and implement correctly.
-    // We recommend Lomuto: pivot = arr[high]
+    // Lomuto partition scheme
+    // Pivot = arr[high]
     template <class T>
     int partition(T* arr, int low, int high) {
-        // TODO
-        (void)arr; (void)low; (void)high;
-        return low;
+        T pivot = arr[high];
+
+        int i = low - 1;
+
+        for (int j = low; j < high; ++j) {
+            if (arr[j] <= pivot) {
+                ++i;
+
+                T temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        T temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
     }
 
     template <class T>
     void quicksort(T* arr, int low, int high) {
-        // TODO
-        (void)arr; (void)low; (void)high;
+        if (low >= high) {
+            return;
+        }
+
+        int pivotIndex = partition(arr, low, high);
+
+        quicksort(arr, low, pivotIndex - 1);
+        quicksort(arr, pivotIndex + 1, high);
     }
 
     // Convenience overload for raw arrays
@@ -30,4 +52,3 @@ namespace dsa {
     }
 
 } // namespace dsa
-
